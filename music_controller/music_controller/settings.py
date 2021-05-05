d16ab9ac1a5a523245530f9308bdcdffeea0dc04
MyName <dsmlguy@gmail.com>
2021-05-05 07:23:10 +0200

    Set hello page

--- music_controller/music_controller/settings.py
+++ music_controller/music_controller/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #add newly created app (called api)
+    'api.apps.ApiConfig',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
